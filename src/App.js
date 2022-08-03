25f06f89019424fddec3a708f38d2975bc3bd596
daijek <daijek080@gmail.com>
2022-08-03 23:48:31 +0100

    fetching tweets from an api and displaying them with the map function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Tweet from "./components/Tweet";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    fetchTweets();
+  });
+
+  const fetchTweets = () => {
+    axios
+      .get(`https://coursera-twitter-api.herokuapp.com/tweets`)
+      .then((data) => {
+        console.log(data);
+        console.log(data.data);
+        setTweets(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <div className="App">
-      <Tweet text="Text passed down" author="Ijezie Daniel" Date="2020-20-15" />
+    <div style={{ background: "#1DA1F2" }} className="App">
+      {tweets.map((tweet, key) => {
+        key = tweet.id;
+
+        return <Tweet key={key} {...tweet} />;
+      })}
+      <Card
+        style={{
+          border: "2px solid white",
+          margin: "0 auto 0 auto",
+          width: "30rem",
+          padding: "5px",
+          color: "black",
+          background: "white",
+        }}
+      >
+        <Card.Body>
+          <Card.Title>I am the react king</Card.Title>
+          <Card.Text>by Ijezie Daniel on 2022-08-03</Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 
 const Tweet = ({ text, author, date }) => {
   return (
     <div>
-      <h3>{text.toUpperCase()}</h3>
-      <h6>
-        by {author} on {date}
-      </h6>
+      <Card
+        style={{
+          border: "2px solid white",
+          margin: "10px auto 30px auto",
+          width: "30rem",
+          padding: "5px",
+          color: "black",
+          background: "white",
+        }}
+      >
+        <Card.Body>
+          <Card.Title>{text.toUpperCase()}</Card.Title>
+          <Card.Text>
+            by {author} on {date}
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
