b1c09fdb84a91c9b7ea00085a142931378c0bf88
daijek <daijek080@gmail.com>
2022-08-04 01:31:10 +0100

    feature to add new tweets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import Tweet from "./components/Tweet";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Card } from "react-bootstrap";
 
 function App() {
-  const [tweets, setTweets] = useState([]);
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = today.getFullYear();
 
+  today = yyyy + "-" + mm + "-" + dd;
+
+  const [tweets, setTweets] = useState([]);
+  const [tweetRefState, setTweetRefState] = useState("");
+  const [authorName, setAuthorName] = useState("");
+  const tweetRef = useRef(null);
+  const authorRef = useRef(null);
   useEffect(() => {
     fetchTweets();
-  });
+  }, []);
+
+  const addTweet = (e) => {
+    e.preventDefault();
+
+    setTweets([
+      { text: tweetRefState, author: authorName, date: today },
+      ...tweets,
+    ]);
+    setAuthorName("");
+    setTweetRefState("");
+  };
 
   const fetchTweets = () => {
     axios
@@ -24,6 +45,47 @@ function App() {
   };
   return (
     <div style={{ background: "#1DA1F2" }} className="App">
+      <form
+        onSubmit={addTweet}
+        style={{
+          width: "30rem",
+          margin: "0 auto 70px auto",
+          paddingTop: "40px",
+        }}
+      >
+        <div class="form-group">
+          <label for="exampleInputEmail1">Tweet here</label>
+          <input
+            type="authorName"
+            name="authorName"
+            value={authorName}
+            class="form-control"
+            placeholder="hey what's your name (because there is no backend here)"
+            ref={authorRef}
+            onChange={() => {
+              setAuthorName(authorRef.current.value);
+            }}
+          />
+          <input
+            type="tweet"
+            name="tweet"
+            value={tweetRefState}
+            class="form-control"
+            placeholder="Whats happening?"
+            ref={tweetRef}
+            onChange={() => {
+              setTweetRefState(tweetRef.current.value);
+            }}
+          />
+          <small id="emailHelp" class="form-text text-muted">
+            Twitter is definately the app for you
+          </small>
+        </div>
+
+        <button type="submit" class="btn btn-primary">
+          Tweet
+        </button>
+      </form>
       {tweets.map((tweet, key) => {
         key = tweet.id;
 
